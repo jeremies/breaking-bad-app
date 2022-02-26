9c3b10bf89f0cccb1c79e3ad47a10a44d12dbce7
Joaquim Monserrat <joaquim.monserrat@techedgegroup.com>
2022-02-26 13:21:20 +0100

    add routing with home and character pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Home } from '../Pages/Home/Home';
+import { CharacterPage } from '../Pages/CharacterPage/CharacterPage';
 
 export function App() {
-  return <div className="App"></div>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/character/:id" element={<CharacterPage />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </HashRouter>
+  );
 }
---
+++ src/components/Pages/CharacterPage/CharacterPage.tsx
@@ -0,0 +1,3 @@
+export function CharacterPage() {
+  return <div>Character Page</div>;
+}
