838dcdda54de3da50ca5735ad0b060329ea6f303
Joaquim Monserrat <jmonserrat@protonmail.com>
2022-03-04 17:37:59 +0100

    adding some character info at the detail page

--- src/components/Pages/CharacterPage/CharacterPage.tsx
+++ src/components/Pages/CharacterPage/CharacterPage.tsx
@@ -3,8 +3,10 @@ import { useParams } from 'react-router-dom';
 import { getCharacter, getRandomQuoteByAuthor } from '../../../services/breaking-bad';
 import { store } from '../../../state/store';
 import { useStore } from '../../../state/storeHooks';
+import { Character } from '../../../types/character';
 import { redirect } from '../../../utils/utils';
 import { initializeCharacterPage, loadCharacter, loadQuote } from './CharacterPage.slice';
+import styles from './CharacterPage.module.css';
 
 export interface Params {
   id: string;
@@ -27,7 +29,7 @@ export function CharacterPage() {
     <Fragment>
       {character.match({
         none: () => <div>Loading character...</div>,
-        some: (character) => <div>{character.name}</div>,
+        some: (character) => <CharacterInfo character={character} />,
       })}
       {quote.match({
         none: () => <div>Loading quote...</div>,
@@ -53,4 +55,36 @@ async function onLoad(id: string) {
 async function getNewRandomQuote(author: string) {
   const quote = await getRandomQuoteByAuthor(author);
   store.dispatch(loadQuote(quote));
+}
+
+function CharacterInfo({ character }: { character: Character }) {
+  return (
+    <div className={styles.container}>
+      <img className={styles.characterImage} src={character.img} />
+      <div className={styles.characterAttributes}>
+        <h1>{character.name}</h1>
+        <Attribute name="Birthday" value={character.birthday} />
+        <ArrayAttribute name="Occupation" value={character.occupation} />
+        <Attribute name="Status" value={character.status} />
+      </div>
+    </div>
+  );
+}
+
+function ArrayAttribute({ name, value }: { name: string; value: Array<string> }) {
+  return (
+    <div className={styles.attribute}>
+      <strong>{name}</strong>
+      <div>{value.join(' | ')}</div>
+    </div>
+  );
+}
+
+function Attribute({ name, value }: { name: string; value: string }) {
+  return (
+    <div className={styles.attribute}>
+      <strong>{name}</strong>
+      <div>{value}</div>
+    </div>
+  );
 }
